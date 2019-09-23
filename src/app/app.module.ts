0f99cc69f1ee81107778cd5481587e469559e8e2
atembamanu <atembaemm07@gmail.com>
2019-09-23 06:03:28 +0300

    update app module withh directives pipe and create-quote component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,12 +9,19 @@ import { AppComponent } from './app.component';
 import { QuoteComponent } from './quote/quote.component';
 import { QuoteDetailsComponent } from './quote/quote-details/quote-details.component';
 
+import { CountDatePipe } from './shared/count-date.pipe';
+import { HighlightBestQuoteDirective } from './highlight-best-quote.directive';
+import { CreateQuoteComponent } from './quote/create-quote/create-quote.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     QuoteComponent,
     QuoteDetailsComponent,
+    CountDatePipe,
+    HighlightBestQuoteDirective,
+    CreateQuoteComponent,
   ],
   imports: [
     NgbModule,
