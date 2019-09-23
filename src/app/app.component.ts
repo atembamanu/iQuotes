8c5be0f48f5ae2f9d0e726a041883dca70f6307d
atembamanu <atembaemm07@gmail.com>
2019-09-23 06:02:18 +0300

    remove modal form from the app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 
 @Component({
@@ -7,22 +6,15 @@ import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
   templateUrl: './app.component.html',
   styles: [`
   #header{margin-bottom  : 40px}
-  #iCreate{ float:right; font-size: 20px;}
-  span#closeModal{font-size: 24px;}
-  .btnIsave{background-color: #f9c128}
   `]
 })
 export class AppComponent implements OnInit {
+
+  constructor() {}
   title = 'iQuote';
-
-  constructor(private modalService: NgbModal) {}
-
-  closeResult: string;
-
-  ngOnInit(): void {}
-
-  open(content) {
-    this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title'});
+  ngOnInit(): void {
   }
+
+
 
 }
